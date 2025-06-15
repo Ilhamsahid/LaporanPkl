824443653d8dea95c7a3ce23955092903c0bfb27
ilhamsahid <ilhamsahit988@gmail.com>
2025-06-15 18:40:36 +0700

    feat: add content to laporan pkl pembimbing role

--- resources/views/pembimbing/pkl/laporan/index.blade.php
+++ resources/views/pembimbing/pkl/laporan/index.blade.php
@@ -15,6 +15,33 @@
                     'deskripsi' => 'Kelola Informasi Laporan PKL',
                 ])
             </header>
+
+            <div class="content">
+
+                <div class="page-section active">
+                    <div class="card">
+                        @include('components.layouts.page-header',[
+                            'judul' => 'Laporan PKL',
+                            'deskripsi' => 'Kelola Laporan PKL siswa yang anda bimbing',
+                            'nama' => 'Laporan',
+                        ])
+
+                        @php($tableVars = getTableVars('laporan.data-table-vars'))
+
+
+                        <x-table.data-table
+                        :rows="$laporans"
+                        :mobileHeaders="$tableVars['mobileHeaders']"
+                        :mobileFields="$tableVars['mobileFields']"
+                        :mobileActions="$tableVars['mobileActions']"
+                        :desktopColumns="$tableVars['desktopColumns']"
+                        :desktopFields="$tableVars['desktopFields']"
+                        :desktopActions="$tableVars['desktopActions']" />
+                        @include('components.layouts.pagination', ['tables' => $laporans])
+                    </div>
+                </div>
+
+            </div>
         </main>
     </div>
 @endsection
