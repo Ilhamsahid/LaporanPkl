fd10cb9f01fd1fd0cb72b05884909345b4726abc
ilhamsahid <ilhamsahit988@gmail.com>
2025-07-06 16:09:26 +0700

    style: minor changes for siswa role

--- app/Models/LaporanPkl.php
+++ app/Models/LaporanPkl.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LaporanPkl extends Model
 {
+    use HasFactory;
     protected $table = 'laporan_pkl';
 
     protected $fillable = [
